c480e8b3df6a95d238186f1e9d4c0d490e31640d
JeffreyHendricks <kiritoichigo1@gmail.com>
2018-10-04 18:09:28 -0400

    MVCMusicStore Constraints Commit

--- MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCMusicStoreApplication.Models
 {
+    [Bind(Exclude="AlbumId")]
+
     public class Album
     {
         public virtual int AlbumId { get; set; }
         public virtual int GenreId { get; set; }
         public virtual int ArtistId { get; set; }
+
+        [Display(Name = "Album Title")]
+        [Required(ErrorMessage ="Album Title cannot be blank")]
         public virtual string Title { get; set; }
+
+        [Display(Name = "Album Price")]
+        [Required]
+        [Range(0.01, 100.00,ErrorMessage ="The price must be btween $0.01 and $100.00")]
+        [DisplayFormat(DataFormatString ="{0:c}")]
         public virtual decimal Price { get; set; }
+
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
+
+        [Display(Name = "Album Art Url")]
         public virtual string AlbumArtUrl {get;set;}
+
         public virtual string AdditonalInfo { get; set; }
         public virtual string CountryOfOrigin { get; set; }
         public virtual bool InStock { get; set; }
