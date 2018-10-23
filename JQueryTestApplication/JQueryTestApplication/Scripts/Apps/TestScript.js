f7e7fcbfcd437a6b33b20432be3e6e2b0eb0c568
JeffreyHendricks <kiritoichigo1@gmail.com>
2018-10-23 18:07:48 -0400

    JQuery Practice application

---
+++ JQueryTestApplication/JQueryTestApplication/Scripts/Apps/TestScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    $("#hide-btn").click(function () {
+        $(".hide_content").hide();
+    });
+
+    $("#show-btn").click(function () {
+        $(".hide_content").show();
+    });
+
+    $("#red-btn").click(function () {
+        $("#mainJQ").children("p").toggleClass("red important");
+    });
+
+    $("#mainJQ").find("a").css({"color":"purple"});
+});
