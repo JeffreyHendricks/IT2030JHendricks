47aee0c6497ed1b6cff10b1a77d0267c506b3af3
JeffreyHendricks <kiritoichigo1@gmail.com>
2018-11-26 15:16:15 -0500

    Bonus Lab 8A

---
+++ Lab8ABonus/Lab8ABonus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab8ABonus.Startup))]
+namespace Lab8ABonus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
